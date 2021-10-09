e90fe48a3e9153589d368e077269562da396eed4
jgjio <johngjackson0@gmail.com>
2021-10-09 09:23:07 -0400

    Make a basic assertion library

---
+++ assertion-library.js
@@ -0,0 +1,26 @@
+const { sum, subtract } = require("./math");
+
+let result, expected;
+
+result = sum(3, 7);
+expected = 10;
+
+expect(result).toBe(expected);
+
+result = subtract(7, 3);
+expected = 4;
+
+expect(result).toBe(expected);
+
+//basic assertion library
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+    toBeGreatedThan(expected) {},
+  };
+}
